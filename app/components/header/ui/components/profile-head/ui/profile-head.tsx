55c8caf83dcdaf67251e5383a5adb7fe34af0991
webskyus <webskyus@gmail.com>
2024-11-13 17:56:46 +0400

    fix build / resolve image import issue

--- app/components/header/ui/components/profile-head/ui/profile-head.tsx
+++ app/components/header/ui/components/profile-head/ui/profile-head.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Image from "next/image";
 import {Icon} from "@iconify/react";
-import Avatar from '@/app/assets/img/profile.jpg';
 
 const ProfileHead = () => {
     return (
         <button className={'flex items-center group'}>
-            <Image src={Avatar.src} width={32} height={32} alt={'Profile Avatar'}/>
+            <Image src={'https://i.imgur.com/DMh83Uy.jpeg'} width={32} height={32} alt={'Profile Avatar'}/>
             <Icon icon="iconoir:nav-arrow-down" width={18} height={18} className={'ml-[5px] group-hover:translate-y-0.5 transition'} />
         </button>
     );
